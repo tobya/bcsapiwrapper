a11bcb1fe2318ea176a375c8d0c035d902de989d
Toby Allen <toby@cookingisfun.ie>
2021-05-04 15:34:22 +0100

    Add BrowseRecipe to Recipe Api Class

--- src/Recipe.php
+++ src/Recipe.php
@@ -153,7 +153,55 @@ class Recipe extends BaseApi
 		} else {
 			return "";
 		}
-	}
+    }
 
-	
+
+    public function BrowsePath($Path)
+    {
+        $Browse = $this->PathsByPath($Path);
+
+        $PathLevel = substr_count($Path, '\\');
+        $NextPathLevel = $PathLevel + 1;
+        //pprint_r($Browse);
+        //exit;
+        //
+
+        $RetrievedPath = $Browse['path'];
+
+
+        if ($RetrievedPath['RecipeCount'] > 0 && $Browse['paths_count'] == 0) {
+            $ListInfo = $this->PathByPathID($p['PathID']);
+
+            $Paths['path'] = $RetrievedPath;
+            $Paths['recipes'] = $ListInfo['recipes'];
+            return $Paths;
+        }
+
+        $Paths = ['parentpath' => $Path, 'parent' => $RetrievedPath, 'children' => [], 'children_count' => ($Browse['paths_count'] - 1)];
+
+        foreach ($Browse['paths'] as $key => $p) {
+            # code...
+
+
+            if (substr_count($p['Path'], '\\') == $NextPathLevel) {
+
+                $Paths['children'][] = $p;
+
+
+                if ($Paths['parent']['RecipeCount'] > 0) {
+
+
+                    $ListInfo = $this->PathByPathID($Paths['parent']['PathID']);
+
+                    $Paths['recipes'] = $ListInfo['recipes'];
+
+                }
+
+                return $Paths;
+
+            }
+
+        }
+    }
 }
+
