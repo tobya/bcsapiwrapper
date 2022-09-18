635dad55d4e6d82b7709bfc50abaff3464cbf996
Toby Allen <toby@cookingisfun.ie>
2022-09-18 08:30:15 +0100

    new get token function

--- src/Store.php
+++ src/Store.php
@@ -8,6 +8,10 @@ class Store extends ApiV4
 {
 
 
+    /**
+     * @param $key
+     * @return array|mixed|object|\Psr\Http\Message\StreamInterface
+     */
     public function  GetKeyValue($key) {
 
          $apipath =   '/{apikey}/keyvalue/get/{key}';
@@ -15,9 +19,25 @@ class Store extends ApiV4
          return $this->CallAPI($apipath, $APIFields);
     }
 
+    /**
+     * @param $key
+     * @param $data
+     * @return array|mixed|object|\Psr\Http\Message\StreamInterface
+     */
     public function SetKeyValue($key,$data) {
          $apipath =   '/{apikey}/keyvalue/set/{key}';
          $APIFields = ['{key}' => $key];
          return $this->CallAPI($apipath, $APIFields, ['info' => $data]);
+    }
+
+    /**
+     * Retrieve data that has been previously set with an identifier token
+     * @param $token
+     * @return array|mixed|object|\Psr\Http\Message\StreamInterface
+     */
+    public function getTokenData($token){
+         $apipath =   '/{apikey}/token/{token}';
+         $APIFields = ['{token}' => $token];
+         return $this->CallAPI($apipath, $APIFields);
     }
 }
