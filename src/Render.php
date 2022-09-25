a519e7780e651476f8ff8428f6f299e90dd2cdd5
Toby Allen <toby@cookingisfun.ie>
2022-09-25 17:35:25 +0100

    add new urltopdf and watermark functions

--- src/Render.php
+++ src/Render.php
@@ -22,4 +22,27 @@ class Render extends BaseApi
 
         return $OutputFilename;
     }
+
+    public function UrlToPDF($url, $OutputFilename){
+
+        \Illuminate\Support\Facades\Http::sink($OutputFilename)
+                                            ->withToken($this->token)
+                                            ->post($this->APIRootURL .  '/v1/urlto/pdf',['url' => $url]);
+
+
+        return $OutputFilename;
+    }
+
+
+    public function UrlToPDFWithWaterMark($url, $watermarkset, $watermarkdata, $OutputFilename){
+
+        $jsondata = json_encode($watermarkdata);
+
+        \Illuminate\Support\Facades\Http::sink($OutputFilename)
+                                            ->withToken($this->token)
+                                            ->post($this->APIRootURL .  '/v1/urlto/pdf/watermark/' . $watermarkset ,['url' => $url, 'data' => $jsondata ]);
+
+
+        return $OutputFilename;
+    }
 }
