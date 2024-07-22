853944a2b843769c272325e79e369c92e28b2e90
Toby Allen <toby@cookingisfun.ie>
2024-07-22 07:40:55 +0100

    new method return all student streams

--- src/Subscription.php
+++ src/Subscription.php
@@ -16,9 +16,28 @@ class Subscription extends ApiV4
          return $this->CallAPI($apipath, $APIFields);
     }
 
+  /**
+   * Get all streams on main subscription.
+   * @return array|mixed|object|\Psr\Http\Message\StreamInterface
+   */
     public function MainSubscriptionStreams(){
       $apipath =   '/v4/subscriptions/2/streams/';
+
          $APIFields = [];
+         return $this->CallAPI($apipath, $APIFields);
+    }
+
+
+  /**
+   * Get all streams available for specific student
+   * @param $individualid
+   * @return array|mixed|object|\Psr\Http\Message\StreamInterface
+   */
+    public function SubscriptionStreamsForStudent($individualid){
+
+      $apipath =   '/v4/subscriptions/streams/for/student/{individualid}';
+
+         $APIFields = ['{individualid}'=>$individualid];
          return $this->CallAPI($apipath, $APIFields);
     }
 
