126ac0e0eb04afee23038877c6a6530210e08449
Toby Allen <toby@cookingisfun.ie>
2025-03-01 20:26:12 +0000

    NEw Media items function forCourse

--- src/Mediaitems.php
+++ src/Mediaitems.php
@@ -8,9 +8,16 @@ class Mediaitems extends ApiV4
 {
 
 
-    public function forCourse($CourseLinkID){
+    public function forCourselink($CourseLinkID){
         $apipath = "/api/v4/courselink/{courselinkid}/mediaitems";
         $APIFields = ['{courselinkid}' => $CourseLinkID];
+        return $this->CallAPI($apipath, $APIFields);
+
+    }
+
+    public function forCourse($CourseID){
+        $apipath = "/api/v4/course/{courseid}/mediaitems";
+        $APIFields = ['{courseid}' => $CourseID];
         return $this->CallAPI($apipath, $APIFields);
 
     }
