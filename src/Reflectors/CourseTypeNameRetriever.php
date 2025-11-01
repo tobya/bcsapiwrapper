6174e951a25d8ce72f31d2871369ba7659c89bf2
Toby Allen <toby@cookingisfun.ie>
2025-11-01 16:32:12 +0000

    explode

--- src/Reflectors/CourseTypeNameRetriever.php
+++ src/Reflectors/CourseTypeNameRetriever.php
@@ -64,7 +64,7 @@ namespace Bcsapi\Reflectors;
       public static function ToCourseTypeName($id, $default = 'None')
       {
            $cn = static::ToConstantName($id,$default);
-            return Str($cn)->split('::')[1];
+            return Str($cn)->explode('::')[1];
       }
 
   }
