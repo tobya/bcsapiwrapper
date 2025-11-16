251363b96274c62c079148bae9308dc5ac0d68ed
Toby Allen <toby@cookingisfun.ie>
2025-11-16 12:16:38 +0000

    note

--- src/Render.php
+++ src/Render.php
@@ -5,6 +5,18 @@ namespace Bcsapi;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 
+/*
+ * Render server can simple use its own connection to DocTo.exe or use the DocTo.works api
+ * To use Render Server simply leave these two .env variables blank
+ * BCSRENDER_V4_DOCTO_APIURL=
+ * BCSRENDER_V4_DOCTO_APITOKEN=
+ *
+ * If these are left blank, Docto.exe will be used.
+ *
+ * If you set the Docto.Works values, they will be used for any conversions.
+ * BCSRENDER_V4_DOCTO_APIURL=http://docto.works/api
+ * BCSRENDER_V4_DOCTO_APITOKEN=gPx1pAzDUQ9lGIuZAHaFe85x9egkgPC10gLiqo9271904fe2
+ */
 class Render extends BaseApi
 {
     protected $token ;
@@ -43,7 +55,7 @@ class Render extends BaseApi
     public function ConvertDocToFormat($DocumentFileName, $format){
         $s = fopen($DocumentFileName,'r');
 
-        Log::debug('DocToPdf ', [$this->DOCTORENDER_URL .  '/v1/Docto/' . $format]);
+
         $OutputFilename = $this->safefilename();
         $response = \Illuminate\Support\Facades\Http::attach('file', $s )
                                             ->sink($OutputFilename)
