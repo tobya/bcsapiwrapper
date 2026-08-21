e2f00e06c66670e8037c0ccdfaee75acc0045c68
Toby Allen <toby@cookingisfun.ie>
2026-08-21 19:47:12 +0100

    updates

--- src/V5/Photo/Requests/GalleryListForYear.php
+++ src/V5/Photo/Requests/GalleryListForYear.php
@@ -3,18 +3,25 @@
 namespace Bcsapi\V5\Photo\Requests;
 
 use Saloon\Enums\Method;
+
+// for caching
+// if you wish to use this you must composer require saloonphp/cache-plugin "^3.0"
 use Illuminate\Support\Facades\Cache;
 use Saloon\CachePlugin\Traits\HasCaching;
+use Saloon\CachePlugin\Contracts\Driver;
 use Saloon\CachePlugin\Contracts\Cacheable;
 use Saloon\CachePlugin\Drivers\LaravelCacheDriver;
-// if you wish to use this you must composer require saloonphp/cache-plugin "^3.0"
-use \Saloon\CachePlugin\Contracts\Driver;
 
 
-class GalleryListForYear extends \Saloon\Http\Request   implements Cacheable
+class GalleryListForYear extends \Saloon\Http\Request  // implements Cacheable
 {
-      // to use  caching uncomment lines and methods and some changes xxx
-       use HasCaching;
+      // CACHING
+      // to use  caching uncomment lines and methods and some changes
+      // [ ] implements
+      // [ ] methods
+      // [ ] has
+
+      // use HasCaching;
 
     /**
      * The HTTP method of the request
@@ -22,8 +29,8 @@ class GalleryListForYear extends \Saloon\Http\Request   implements Cacheable
     protected Method $method = Method::GET;
 
 
-    public function __construct(
-           public string $year,
+    public function __construct(  
+           public string $year, 
     )
     {  }
 
@@ -45,16 +52,15 @@ class GalleryListForYear extends \Saloon\Http\Request   implements Cacheable
 * If you wish to implement caching , you can uncomment these two methods, the implements and has statements above.
 */
 
-
-    public function resolveCacheDriver(): \Saloon\CachePlugin\Contracts\Driver
-    {
+/*
+    public function resolveCacheDriver(): Driver
+     {
          return new LaravelCacheDriver(Cache::store(config('cache.default')));
      }
 
      public function cacheExpiryInSeconds(): int
      {
-
          return 300;
      }
-
+*/
 }
--- src/V5/Photo/Requests/RandomImage.php
+++ src/V5/Photo/Requests/RandomImage.php
@@ -3,13 +3,24 @@
 namespace Bcsapi\V5\Photo\Requests;
 
 use Saloon\Enums\Method;
+
+// for caching
+// if you wish to use this you must composer require saloonphp/cache-plugin "^3.0"
+use Illuminate\Support\Facades\Cache;
 use Saloon\CachePlugin\Traits\HasCaching;
+use Saloon\CachePlugin\Contracts\Driver;
 use Saloon\CachePlugin\Contracts\Cacheable;
+use Saloon\CachePlugin\Drivers\LaravelCacheDriver;
 
 
 class RandomImage extends \Saloon\Http\Request  // implements Cacheable
 {
-      // to use  caching uncomment lines and methods and some changes xxx
+      // CACHING
+      // to use  caching uncomment lines and methods and some changes
+      // [ ] implements
+      // [ ] methods
+      // [ ] has
+
       // use HasCaching;
 
     /**
@@ -19,9 +30,9 @@ class RandomImage extends \Saloon\Http\Request  // implements Cacheable
 
 
     public function __construct(  
-           public ?string $year = null,
-           public ?string $month = null,
-           public ?string $day = null,
+           public string $year, 
+           public string $month, 
+           public string $day, 
     )
     {  }
 
@@ -31,22 +42,8 @@ class RandomImage extends \Saloon\Http\Request  // implements Cacheable
      */
     public function resolveEndpoint(): string
     {
-         $apipath =   'api/v2/images/random/';
-         $fields = [];
-         if ($this->year > -1 ){
-            $apipath .= '{year}/';
-
-         }
-         if ($this->month > -1 ){
-            $apipath .= '{month}/';
-
-         }
-         if ($this->day > -1 ){
-            $apipath .= '{day}/';
-
-         }
-
-         return str($apipath)->replace(
+         return str('api/v2/images/random/{year}/{month}/{day}')
+                             ->replace(
                                     ['{year}','{year?}','{month}','{month?}','{day}','{day?}'],
                                     [$this->year, $this->year,$this->month, $this->month,$this->day, $this->day]
                               );
