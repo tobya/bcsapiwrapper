6900e25755576d69f9f775b3c3020260e830f259
Toby Allen <toby@cookingisfun.ie>
2026-09-13 15:38:05 +0100

    use v5

--- src/V5/Photo/PhotoConnector.php
+++ src/V5/Photo/PhotoConnector.php
@@ -15,7 +15,7 @@ class PhotoConnector extends Connector
      */
     public function resolveBaseUrl(): string
     {
-        return (string) Url::fromString(config('bcsapi.v2.demophoto.url'))->withPath('/');
+        return (string) Url::fromString(config('bcsapi.v5.demophoto.url'))->withPath('/');
     }
 
     /**
